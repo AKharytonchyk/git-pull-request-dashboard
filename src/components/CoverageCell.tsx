cc089b6c166b1a0289eba44eeb9d14c6a640f2f9
Artsiom Kharytonchyk <artsiom.kharytonchyk@coxautoinc.com>
2024-04-21 16:31:07 +0300

    fix: remove debug code

--- src/components/CoverageCell.tsx
+++ src/components/CoverageCell.tsx
@@ -7,17 +7,28 @@ export const CoverageCell: React.FC<{ coverage: number; previous: number }> = ({
   coverage,
   previous,
 }) => {
-  const randomPrevios0to100 = Math.floor(Math.random() * 100);
   const icon = React.useMemo(
-    () => CoverageDiffIcon(coverage - randomPrevios0to100, randomPrevios0to100),
-    [coverage, randomPrevios0to100]
+    () => CoverageDiffIcon(coverage - previous, previous),
+    [coverage, previous]
   );
 
   const styles = React.useMemo(() => RYGGradient(coverage), [coverage]);
   return (
     <TableCell sx={styles}>
-      <Box sx={{ display: "flex", justifyContent: "space-between", alignItems: "baseline", maxWidth: "4.5em" }}>
-        <Typography variant="body1">{Math.round(coverage)}%</Typography>
+      <Box
+        sx={{
+          display: "flex",
+          justifyContent: "space-between",
+          alignItems: "baseline",
+          maxWidth: "4.5em",
+        }}
+      >
+        <Typography
+          variant="body1"
+          sx={{ fontWeight: coverage < 50 ? "bolder" : "bold" }}
+        >
+          {Math.round(coverage)}%
+        </Typography>
         {icon}
       </Box>
     </TableCell>
